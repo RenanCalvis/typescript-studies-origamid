564872c00f47cb255e4a24268ced2d15cdc898e5
renancalvis <renancalvis@gmail.com>
2026-08-12 13:59:58 -0400

    feat: exercice annotation and inferece lint errors

---
+++ annotation-and-inference/script.js
@@ -0,0 +1,21 @@
+"use strict";
+const input = document.querySelector('input');
+const total = localStorage.getItem('total');
+if (total && input) {
+    input.value = total;
+    calcularGanho(Number(input.value));
+}
+function calcularGanho(value) {
+    const p = document.querySelector('p');
+    if (p)
+        p.innerText = `ganho total: ${value + 100 - value * 0.2}`;
+}
+function totalMudou() {
+    if (input) {
+        const value = Number(input.value);
+        localStorage.setItem('total', value.toString());
+        calcularGanho(value);
+    }
+}
+if (input)
+    input.addEventListener('keyup', totalMudou);
